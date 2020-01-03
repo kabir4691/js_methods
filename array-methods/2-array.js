9d48e65048b251d4d22e2498a293c7119b9504b0
Kabir <kabir4691@gmail.com>
2020-01-03 17:55:32 +0530

    complete assignments

--- array-methods/2-array.js
+++ array-methods/2-array.js
@@ -9,22 +9,34 @@ var words = [
 ];
 //Write a function findLongestWord that takes an array of words and returns the longest one.
 //If there are 2 with the same length, it should return the first occurrence.
-
-
+function findLongestWord(words) { 
+  return words.reduce(function (final, current) { 
+    return final.length > current.length ? final : current;
+  });
+}
+console.log(findLongestWord(words));
 
 
 var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 // Create a sumArray function that takes an array of numbers1 as a parameter, and calculate the sum of all its numbers
 // Use reduce method of array
 // Use the above sum and calculate the average.
-
-
+function sumArray(numbers) {
+	return numbers.reduce(function(final, current) {
+    return final + current;
+	}, 0);
+}
+console.log(sumArray(numbers1));
 
 
 var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
-
-
+function averageNumbers(numbers) {
+	return numbers.reduce(function(final, current) {
+		return final + current;
+	}, 0) / numbers.length;
+}
+console.log(averageNumbers(numbers2));
 
 
 var words2 = [
@@ -40,6 +52,14 @@ var words2 = [
   'palace'
 ];
 //Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
+function averageWordLength(words) {
+	return (
+		words.reduce(function(final, current) {
+			return final + current.length;
+		}, 0) / words.length
+	);
+}
+console.log(averageWordLength(words2));
 
 
 
--- array-methods/4-array.js
+++ array-methods/4-array.js
@@ -30,6 +30,13 @@ var data = [
 // 1 human year = 7 dog year
 
 // your code goes here
+let sum = 0;
+for (item of data) {
+  if (item.type === "dog") {
+   sum += item.age * 7; 
+  }
+}
+console.log(sum);
 
 // Solution is 105
 
@@ -37,5 +44,17 @@ var data = [
 // 1. filter - for filtering the cat or dog
 // 2. map - to multiply human year to dog year
 // 3. reduce - to accumulate total age.
+console.log(data.filter(item => {
+  return item.type === "dog";
+})
+  .map(item => {
+    item.age *= 7;
+    return item;
+})
+  .reduce(function (previous, current) {
+  previous += current.age;
+  return previous;
+  }, 0)
+);
 
 // Solution 105
